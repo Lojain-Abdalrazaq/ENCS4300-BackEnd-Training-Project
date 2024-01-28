ddbbf70317e130650f4231ac3505bcf7b2326491
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-01-28 15:20:26 +0200

    Making the error message well-formatted and more readable.

--- src/main/java/com/example/training/Handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/training/Handler/GlobalExceptionHandler.java
@@ -2,7 +2,10 @@ package com.example.training.Handler;
 
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,6 +20,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolation(ConstraintViolationException ex, WebRequest request) {
+        List<String> errorMessages = ex.getConstraintViolations()
+                .stream()
+                .map(violation -> violation.getMessage())
+                .collect(Collectors.toList());
+
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), errorMessages.toString(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
     // used for any other exceptions types, such as invalid input
     @ExceptionHandler(Exception.class)
